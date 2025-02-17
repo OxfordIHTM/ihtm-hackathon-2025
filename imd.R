610e7d73f85afdd071b0e5abab274003f282bd20
ibrahimajami <ibrahimajami2@gmail.com>
2025-02-17 13:03:24 +0000

    Individual work

--- imd.R
+++ imd.R
@@ -1,4 +1,11 @@
-imd <- read_data(
-  url = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", 
-  destfile = "data/imd.csv"
+###Downloading and reading the file
+
+download.file(url = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", 
+              destfile = "data/imd.csv"
 )
+read.csv("data/imd.csv")
+View("data/imd.csv")
+imd <- read.csv("data/imd.csv")
+View(imd)
+
+## Function for imd data
