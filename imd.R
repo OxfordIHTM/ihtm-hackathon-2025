0b3dc878ddf2b8ad44127cd9840a5a29f2e63436
Onesmus Odongo Onyango <onesmusodongo@gmail.com>
2025-02-17 12:53:44 +0000

    Readind imd

--- imd.R
+++ imd.R
@@ -1,4 +1,6 @@
-imd <- read_data(
-  url = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", 
-  destfile = "data/imd.csv"
+imd = read.csv(
+  file = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", 
+  
 )
+imd
+summary(imd) #Summarising the data
