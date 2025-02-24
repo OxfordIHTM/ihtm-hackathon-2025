dfeb8b978ab251e32be09e61b9b52cb283e7310f
seiza miyazaki <glay94525@gmail.com>
2025-02-24 11:56:23 +0000

    First successful Debug

--- 01_read_data.R
+++ 01_read_data.R
@@ -15,7 +15,7 @@ if (!"imd.csv" %in% list.files("data")) {
 }
 
 ### Read imd.csv dataset ----
-imd <- read.csv("imd.csv")
+imd <- read.csv("data/imd.csv")
 
 
 ## England map dataset ----
@@ -43,6 +43,6 @@ library(sf)
 
 england <- st_read(
   dsn = "data/English IMD 2019/IMD_2019.shp",
-  layer = "IMD_2019.shp"
+  layer = "data/English IMD 2019/IMD_2019.shp"
 )
 
