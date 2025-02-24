c9cd2301a05bea378294e6a7088f793422f59bbe
Angela Ying Xiao <angela.xiao@ndm.ox.ac.uk>
2025-02-24 12:24:54 +0000

    Fix easter egg

--- imd.R
+++ imd.R
@@ -1,7 +1,11 @@
 # Index of Multiple Deprivation ------------------------------------------------
 
+## Read shapefile of England ----
+library(sf)
+
 ## download and read data ----
 source("01_read_data.R")
 
 ## process data ----
 source("02_process_data.R")
+
