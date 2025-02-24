6096f90acbcfad8e46ba8ab2334dcf3a6cde8de5
Nguyen Quan Nhu Hao <nguyenquannhuhao@gmail.com>
2025-02-24 08:33:48 +0000

    Work on Issue #5

--- imd.R
+++ imd.R
@@ -1,14 +1,10 @@
-download.file("data.csv")
-
-imd <- read_data(
-  url = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", 
-  destfile = "data/imd.csv"
-)
-
-
 # download and read data
 
-download.file (url = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv",
-destfile= "data/imd.csv"
+if (!"imd.csv" %in% list.files("data")){ 
+  download.file (url = "https://assets.publishing.service.gov.uk/media/5dc407b440f0b6379a7acc8d/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv",
+                  destfile= "data/imd.csv"
 )
+} else {
+  message("File already downloaded!")
+}
 imd <- read.csv("data/imd.csv")
