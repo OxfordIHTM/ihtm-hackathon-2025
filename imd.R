c959301154a427ead72a8a5cd8a540475d2d3115
Onesmus Odongo Onyango <onesmusodongo@gmail.com>
2025-03-17 08:55:18 +0000

    Scatter plot: Income children score vs crime score

--- imd.R
+++ imd.R
@@ -10,3 +10,15 @@ source("01_read_data.R")
 
 ## process data ----
 source("02_process_data.R")
+imd
+
+colnames(imd)
+library(ggplot2)
+
+#Scatter plot
+ggplot(imd, aes(x = crime_score, y = income_children_score)) +
+  geom_point(size=3) +
+  labs(title = "Income children score vs. Crime score", 
+       x = "Crime score", 
+       y = "Income children Score",
+       color= "income_children_score")
